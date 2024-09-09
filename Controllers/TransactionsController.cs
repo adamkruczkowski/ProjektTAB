749f5a85462b195134419d8891b208a0f2b521c0
Łukasz Jarząb <lukasz.jarzab01@gmail.com>
2024-09-09 23:01:04 +0200

    added: DownloadPDF

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -1,10 +1,17 @@
 ﻿using AutoMapper;
+using iTextSharp.text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjektTabAPI.Data;
 using ProjektTabAPI.Entities.Domain;
 using ProjektTabAPI.Entities.Dtos.Transaction;
 using ProjektTabAPI.Repositories;
+using System.Reflection.Metadata;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Org.BouncyCastle.Tls;
 
 namespace ProjektTabAPI.Controllers
 {
@@ -127,6 +134,82 @@ namespace ProjektTabAPI.Controllers
             }
             var transactionsDto = mapper.Map<List<TransactionDto>>(transactions);
             return Ok(transactionsDto);
+        }
+
+        [HttpGet]
+        [Route("GeneratePDF/{BA_id:Guid}")]
+        public async Task<IActionResult> GeneratePDF([FromRoute] Guid BA_id)
+        {
+            var transactions = await transactionRepository.GetAllByBAId(BA_id);
+            if (transactions is null)
+            {
+                return NotFound();
+            }
+            transactions.Sort((x, y) => DateTime.Compare(y.CreatedAt, x.CreatedAt));
+            var transactionsDto = mapper.Map<List<TransactionDto>>(transactions);
+
+            var client = await clientRepository.GetClientById(BA_id);
+            var bankingAcc = await bankingRepository.GetById(BA_id);
+
+            string fontPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "arial.ttf");
+
+            // Generowanie PDF
+            using (var stream = new MemoryStream())
+            {
+                iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document(PageSize.A4, 10f, 10f, 20f, 20f);
+                PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+
+                BaseFont baseFont = BaseFont.CreateFont(fontPath, BaseFont.CP1250, BaseFont.EMBEDDED);
+                Font font = new Font(baseFont, 12, Font.NORMAL);
+
+                pdfDoc.Add(new Paragraph("PolBank"));
+                pdfDoc.Add(new Paragraph("Wyciąg z konta", font));
+                pdfDoc.Add(new Paragraph($"Wygenerowano: {DateTime.Now}"));
+                pdfDoc.Add(new Paragraph("\n"));
+                if (transactions[0].Sender_BAId == BA_id)
+                {
+                    pdfDoc.Add(new Paragraph($"Klient: {transactions[0].Sender.Client.Name + " " + transactions[0].Sender.Client.Surname}", font));
+                    pdfDoc.Add(new Paragraph($"Email klienta: {transactions[0].Sender.Client.Email}", font));
+                }
+                else
+                {
+                    pdfDoc.Add(new Paragraph($"Klient: {transactions[0].Recipient.Client.Name + transactions[0].Recipient.Client.Surname}", font));
+                    pdfDoc.Add(new Paragraph($"Email klienta: {transactions[0].Recipient.Client.Email}", font));
+                }
+                pdfDoc.Add(new Paragraph($"Id konta bankowego: {bankingAcc.Id}"));
+                pdfDoc.Add(new Paragraph($"Numer konta bankowego: {bankingAcc.Number}"));
+                pdfDoc.Add(new Paragraph($"Stan konta bankowego: {bankingAcc.Amount} zł", font));
+                pdfDoc.Add(new Paragraph("\n"));
+                pdfDoc.Add(new Paragraph("\n"));
+
+                PdfPTable table = new PdfPTable(7); // 7 kolumn
+                table.AddCell(new PdfPCell(new Phrase("ID transakcji", font)));
+                table.AddCell(new PdfPCell(new Phrase("Kwota", font)));
+                table.AddCell(new PdfPCell(new Phrase("Tytuł", font)));
+                table.AddCell(new PdfPCell(new Phrase("Data przelewu", font)));
+                table.AddCell(new PdfPCell(new Phrase("Stan konta przed operacją", font)));
+                table.AddCell(new PdfPCell(new Phrase("Stan konta po operacji", font)));
+                table.AddCell(new PdfPCell(new Phrase("ID konta nadawcy", font)));
+                table.AddCell(new PdfPCell(new Phrase("ID konta odbiorcy", font)));
+
+                foreach (var transaction in transactionsDto)
+                {
+                    table.AddCell(new PdfPCell(new Phrase(transaction.Id.ToString(), font)));
+                    table.AddCell(new PdfPCell(new Phrase(transaction.Amount.ToString("C"), font)));
+                    table.AddCell(new PdfPCell(new Phrase(transaction.Title, font)));
+                    table.AddCell(new PdfPCell(new Phrase(transaction.CreatedAt.ToString("g"), font)));
+                    table.AddCell(new PdfPCell(new Phrase(transaction.Balance_before.ToString("C"), font)));
+                    table.AddCell(new PdfPCell(new Phrase((transaction.Balance_before + transaction.Amount).ToString("C"), font)));
+                    table.AddCell(new PdfPCell(new Phrase(transaction.Sender.Id.ToString(), font)));
+                    table.AddCell(new PdfPCell(new Phrase(transaction.Recipient.Id.ToString(), font)));
+                }
+
+                pdfDoc.Add(table);
+                pdfDoc.Close();
+
+                return File(stream.ToArray(), "application/pdf", "Transactions_Report.pdf");
+            }
         }
     }
 }
